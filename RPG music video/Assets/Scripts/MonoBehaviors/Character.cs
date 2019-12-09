bf3fce166d2e6c7593dab238cb4a60abc2f9edb3
Jay Ackley <jayackley@gmail.com>
2019-12-08 19:35:03 -0500

    First Upload

---
+++ RPG music video/Assets/Scripts/MonoBehaviors/Character.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+//1
+public abstract class Character : MonoBehaviour
+{
+
+    //2
+    public HitPoints hitPoints;
+    public float maxHitPoints;
+    public float startingHitPoints;
+
+}
---
+++ RPG music video/Assets/Scripts/MonoBehaviors/Player.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class Player : Character
+{
+    public HealthBar healthBarPrefab;
+    HealthBar healthBar;
+
+    public Inventory inventoryPrefab;
+    Inventory inventory;
+
+    public void Start()
+    {
+        hitPoints.value = startingHitPoints;
+        healthBar = Instantiate(healthBarPrefab);
+        healthBar.character = this;
+        inventory = Instantiate(inventoryPrefab);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("CanBePickedUp"))
+        {
+            Item hitObject = collision.gameObject.GetComponent<Consumable>().item;
+            if (hitObject != null)
+            {
+                bool shouldDisappear = false;
+                switch (hitObject.itemType)
+                {
+
+                    case Item.ItemType.COIN:
+                        shouldDisappear = inventory.AddItem(hitObject);
+                        shouldDisappear = true;
+                        break;
+                    case Item.ItemType.HEALTH:
+                        shouldDisappear = AdjustHitPoints(hitObject.quantity);
+                        break;
+                    default:
+                        break;
+                }
+
+                if (shouldDisappear)
+                {
+                    collision.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+    public bool AdjustHitPoints(int amount)
+    {
+        if (hitPoints.value < maxHitPoints)
+        {
+            hitPoints.value = hitPoints.value + amount;
+            print("Adjusted Hit Points by: " + amount + ". New value: " + hitPoints.value);
+            return true;
+        }
+        print("didn't adjust hitpoints");
+        return false;
+    }
+}
+
